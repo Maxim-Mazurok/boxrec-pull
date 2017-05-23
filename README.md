6f1f3deaaeca747adce797a4e5c4f2420e3068a8
Maxim Mazurok <maxim@mazurok.com>
2017-05-24 02:41:51 +0300

    findAllByName + findByFight + getScheduleById + readme update

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # Boxrec-Pull
 Allows for information about a boxer to be pulled from the online boxing database, Boxrec.
 
-[![NPM version](https://badge.fury.io/js/boxrec-pull.svg)](http://badge.fury.io/js/boxrec-pull)
+## The purpose of my fork
+My forks goal is to compare two boxers, participating in one fight identified by date.
 
+Also, my fork gets more data to make fair comparison:
+- name
+- nickname
+- record (w, l, d)
+- rating (%)
+- ranking (world and local)
+- birthday
+- age
+- ... more coming soon
 
 ## Installation
 ```sh
-npm install boxrec-pull --save
+npm install Maxim-Mazurok/boxrec-pull --save
 ```
 
 ## Usage
@@ -20,9 +30,34 @@ boxrec.findById(356831, function(err, boxer) {
 boxrec.findByName('Carl Froch', function(err, boxer) {
   console.log(boxer.nickname); // 'The Cobra'
 });
+
+boxrec.findByFight('Fred Evans', 'Najim Fennan', new Date(Date.UTC(2017, 4, 26)).getTime(), function(err, fight) {
+    console.log(fight);
+    /**
+    [ { name: 'Fred Evans',
+        nickname: '',
+        record: { w: 0, l: 0, d: 0 },
+        rating: null,
+        ranking: 
+           { world: { rank: null, total: null },
+             local: { rank: null, total: null, country_code: null } },
+        birthday: '1991-02-04',
+        age: 26 },
+      { name: 'Najim Fennane',
+        nickname: '',
+        record: { w: 1, l: 1, d: 0 },
+        rating: '10%',
+        ranking: 
+           { world: { rank: 769, total: 1802 },
+             local: { rank: 3, total: 6, country_code: 'BE' } },
+        birthday: '1985-09-02',
+        age: 31 } ]
+    **/
+});
+
 ```
 ## Notes
-At present only the name, record, and nickname for a fighter are returned.
+It seems like boxrec have access limits for unregistered users. So, if script stops working, try to open boxrec in your browser. If it says "Please login to continue" on every page - seems like you are over limit. You can tweak COOKIE and UA variable in source code to get access to boxrec again.
 
 ## Tests
 ```sh
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-var request = require('request');
-var cheerio = require('cheerio');
+const request = require('request');
+const http = require('http');
+const querystring = require('querystring');
+const cheerio = require('cheerio');
+
+/* SET THEM IF YOU ARE OVER NO-LOGIN LIMIT */
+const UA = '';
+const COOKIE = '';
 
 // https://stackoverflow.com/a/21984136/4536543
 function _calculateAge(birthday) {
@@ -36,9 +42,60 @@ function extractInfo(data) {
     };
 }
 
+function extractScheduleInfo(data) {
+    var $ = cheerio.load(data);
+    var ret = [];
+    var schedule_elem = null;
+
+    $('h2.pageTitle').each(function() {
+        if ($(this).text().trim().toLowerCase() === 'schedule' && schedule_elem === null) {
+            schedule_elem = $(this);
+        }
+    });
+
+    if (schedule_elem !== null) {
+        $(schedule_elem).next().find('.tBoutList').each(function () {
+            var date = Date.parse($(this).find('td.profileDateWidth > a.dLink').text().trim());
+            var name = $(this).find('.boxerLink').text().trim();
+            var link = $(this).find('.boxerLink').attr('href');
+            var id = parseInt(link.substr(link.indexOf('boxer/') + 'boxer/'.length));
+            ret.push({
+                id: id,
+                name: name,
+                date: date
+            })
+        })
+    }
+
+    return ret;
+}
+
+function extractInfoFromSearch(data) {
+    var $ = cheerio.load(data);
+    var ret = [];
+
+    $('.tBoutList').each(function () {
+        var name = $(this).find('tr.bTop > td > a.boxerLink').text().trim();
+        var link = $(this).find('tr.bTop > td > a.boxerLink').attr('href');
+        var id = parseInt(link.substr(link.indexOf('boxer/') + 'boxer/'.length));
+        ret.push({
+            id: id,
+            name: name
+        })
+    });
+
+    return ret;
+}
+
 module.exports = {
     findById: function (id, cb) {
-        request('http://boxrec.com/boxer/' + id, function (err, response, body) {
+        request({
+            headers: {
+                'Cookie': COOKIE,
+                'User-Agent': UA
+            },
+            uri: 'http://boxrec.com/boxer/' + id
+        }, function (err, response, body) {
             if (typeof id !== 'number') return cb(true);
             return cb(null, extractInfo(body));
         });
@@ -53,5 +110,74 @@ module.exports = {
                 return cb(null, extractInfo(body));
             });
         });
+    },
+
+    findAllByName: function(name, cb) {
+        var post_data = querystring.stringify({
+            'status': 'all',
+            'search_text' : name
+        });
+        var post_options = {
+            host: 'boxrec.com',
+            port: '80',
+            path: '/search/do_search',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(post_data),
+                'Cookie': COOKIE,
+                'User-Agent': UA
+            }
+        };
+        var post_req = http.request(post_options, function(res) {
+            res.setEncoding('utf8');
+            var body = '';
+            res.on('data', function(d) {
+                body += d;
+            });
+            res.on('end', function() {
+                return cb(null, extractInfoFromSearch(body));
+            })
+        });
+
+        post_req.write(post_data);
+        post_req.end();
+    },
+
+    getScheduleById: function (id, cb) {
+        request({
+            headers: {
+                'Cookie': COOKIE,
+                'User-Agent': UA
+            },
+            uri: 'http://boxrec.com/boxer/' + id
+        }, function (err, response, body) {
+            return cb(null, extractScheduleInfo(body));
+        });
+    },
+
+    findByFight: function(name1, name2, date, cb) {
+        module.exports.findAllByName(name1, function(error, boxers) {
+            boxers.forEach(function (boxer) {
+                module.exports.getScheduleById(boxer.id, function (error, schedule) {
+                    schedule.forEach(function (fight) {
+                        if ((
+                            fight.name.toLowerCase().indexOf(name2.toLowerCase()) > -1
+                            || name2.toLowerCase().indexOf(fight.name.toLowerCase()) > -1
+                            || fight.name.toLowerCase().split(' ')[0].indexOf(name2.toLowerCase().split(' ')[0]) > -1
+                            || name2.toLowerCase().split(' ')[0].indexOf(fight.name.toLowerCase().split(' ')[0]) > -1
+                            || fight.name.toLowerCase().split(' ')[1].indexOf(name2.toLowerCase().split(' ')[1]) > -1
+                            || name2.toLowerCase().split(' ')[1].indexOf(fight.name.toLowerCase().split(' ')[1]) > -1
+                        ) && Math.abs(fight.date - date) <= 1000 * 60 * 60 * 24) {
+                            module.exports.findById(boxer.id, function (error, data1) {
+                                module.exports.findById(fight.id, function (error, data2) {
+                                    return cb(null, [data1, data2]);
+                                })
+                            })
+                        }
+                    })
+                });
+            })
+        })
     }
-}
+};
