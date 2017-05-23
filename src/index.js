16ed7c8c49f945a309f185db47282b12986b661f
Maxim Mazurok <maxim@mazurok.com>
2017-05-23 23:16:40 +0300

    idea ignore + ranking

--- src/index.js
+++ src/index.js
@@ -2,37 +2,38 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 function extractInfo(data) {
-  var $ = cheerio.load(data);
+    var $ = cheerio.load(data);
 
-  var boxer = {
-    name: $('.boxerTitle').first().text().trim(),
-    nickname: $('span[itemprop="alternateName"]').text(),
-    record: {
-      w: $('.bgwonBlock').first().text(),
-      l: $('.bglostBlock').first().text(),
-      d: $('.bgdrawBlock').first().text()
-    }
-  };
-
-  return boxer;
+    return {
+        name: $('.boxerTitle').first().text().trim(),
+        nickname: $('span[itemprop="alternateName"]').text(),
+        record: {
+            w: $('.bgwonBlock').first().text(),
+            l: $('.bglostBlock').first().text(),
+            d: $('.bgdrawBlock').first().text()
+        },
+        ranking: {
+            world: $('div.flag.world[title="World"] + div > a').text().trim()
+        }
+    };
 }
 
 module.exports = {
-  findById: function(id, cb) {
-    request('http://boxrec.com/boxer/' + id, function(err, response, body) {
-      if (typeof id !== 'number') return cb(true);
-      return cb(null, extractInfo(body));
-    });
-  },
+    findById: function (id, cb) {
+        request('http://boxrec.com/boxer/' + id, function (err, response, body) {
+            if (typeof id !== 'number') return cb(true);
+            return cb(null, extractInfo(body));
+        });
+    },
 
-  findByName: function(name, cb) {
-    request('http://www.google.com/search?q=' + name + '%20site%3Aboxrec.com', function(err, response, body) {
-      var $ = cheerio.load(body);
-      var id = $('cite').first().text().slice(17);
+    findByName: function (name, cb) {
+        request('http://www.google.com/search?q=' + name + '%20site%3Aboxrec.com', function (err, response, body) {
+            var $ = cheerio.load(body);
+            var id = $('cite').first().text().slice(17);
 
-      request('http://boxrec.com/boxer/' + id, function(err, response, body) {
-        return cb(null, extractInfo(body));
-      });
-    });
-  }
+            request('http://boxrec.com/boxer/' + id, function (err, response, body) {
+                return cb(null, extractInfo(body));
+            });
+        });
+    }
 }
